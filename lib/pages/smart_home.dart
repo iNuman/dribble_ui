922a122cef77c596553bf1d9c216fbf4191da86b
Noman Ali <132977453+space551@users.noreply.github.com>
2024-12-24 17:31:11 +0500

    Icons added

--- lib/pages/smart_home.dart
+++ lib/pages/smart_home.dart
@@ -1,3 +1,4 @@
+import 'package:dribble_ui/widgets/smart_device_card_widget.dart';
 import 'package:flutter/material.dart';
 // import 'package:google_fonts/google_fonts.dart';
 
@@ -21,57 +22,87 @@ class _SmartHomeState extends State<SmartHome> {
     ["Smart Fan", "lib/icons/fan.png", false],
   ];
 
+  void powerSwitchChanged(bool value, int index) {
+    setState(() {
+      mySmartDevices[index][2] = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset(
-                  'lib/icons/menu.png',
-                  height: 45,
-                  color: Colors.grey[800],
-                ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                'lib/icons/menu.png',
+                height: 45,
+                color: Colors.grey[800],
+              ),
 
-                // account Icon
-                Icon(
-                  Icons.person,
-                  size: 45,
-                  color: Colors.grey.shade800,
-                )
+              // account Icon
+              Icon(
+                Icons.person,
+                size: 45,
+                color: Colors.grey.shade800,
+              )
+            ],
+          ),
+
+          const SizedBox(height: 20),
+
+          // welcome home
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Welcome Home,",
+                  style: TextStyle(fontSize: 20, color: Colors.grey.shade800),
+                ),
+                const Text(
+                  'Noman Ali',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
               ],
             ),
+          ),
 
-            const SizedBox(height: 20),
+          const SizedBox(height: 25),
 
-            // welcome home
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Welcome Home,",
-                    style: TextStyle(fontSize: 20, color: Colors.grey.shade800),
-                  ),
-                  const Text(
-                    'Noman Ali',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40.0),
+            child: Divider(
+              thickness: 1,
+              color: Color.fromARGB(255, 204, 204, 204),
             ),
+          ),
 
-            const SizedBox(height: 25),
-          ],
-        ),
+          const SizedBox(height: 25),
+
+          Expanded(
+              child: GridView.builder(
+                  itemCount: 4,
+                  physics: const NeverScrollableScrollPhysics(),
+                  padding: const EdgeInsets.symmetric(horizontal: 18),
+                  gridDelegate:
+                      const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 1 / 1.3),
+                itemBuilder: (context, index) {
+                  return SmartDeviceCardWidget(
+                    smartDeviceName: mySmartDevices[index][0],
+                    iconPath: mySmartDevices[index][1],
+                    powerOn: mySmartDevices[index][2],
+                    onChanged: (value) => powerSwitchChanged(value, index),
+                  );
+                },)),
+        ],
       ),
     );
   }
+
 }
