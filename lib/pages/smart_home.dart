407b27ba3b2388264ff6d22543bf09b363b8bc98
Noman Ali <132977453+space551@users.noreply.github.com>
2024-12-24 16:18:33 +0500

    Icons added

--- lib/pages/smart_home.dart
+++ lib/pages/smart_home.dart
@@ -8,13 +8,28 @@ class SmartHome extends StatefulWidget {
 }
 
 class _SmartHomeState extends State<SmartHome> {
+
+  final double horizontalPadding = 40;
+  final double verticalPadding = 25;
+
+  // list of smart devices
+  List mySmartDevices = [
+    // [ smartDeviceName, iconPath , powerStatus ]
+    ["Smart Light", "lib/icons/light-bulb.png", true],
+    ["Smart AC", "lib/icons/air-conditioner.png", false],
+    ["Smart TV", "lib/icons/smart-tv.png", false],
+    ["Smart Fan", "lib/icons/fan.png", false],
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.blueAccent,
-        child: const Center(child: Text("Smart Home")),
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
 
+        ],
       ),
     );
   }
