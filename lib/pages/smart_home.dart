8df56c763da1fba17759961dfda434588db0309a
Noman Ali <132977453+space551@users.noreply.github.com>
2024-12-24 16:00:53 +0500

    Bottom Navigation setup

--- lib/pages/smart_home.dart
+++ lib/pages/smart_home.dart
@@ -14,6 +14,7 @@ class _SmartHomeState extends State<SmartHome> {
       child: Container(
         color: Colors.blueAccent,
         child: const Center(child: Text("Smart Home")),
+
       ),
     );
   }
