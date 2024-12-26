8364ffc89b314c6ed7cc626916d5b02c83834b20
Noman Ali <132977453+space551@users.noreply.github.com>
2024-12-26 17:09:59 +0500

    User Profile

--- lib/pages/user_profile.dart
+++ lib/pages/user_profile.dart
@@ -34,7 +34,7 @@ class UserProfile extends StatelessWidget {
                   // setState(() {});
                 },
               ),
-              SizedBox(height: 24),
+              const SizedBox(height: 24),
               buildName(MockData.userData),
 
               const SizedBox(height: 24),
