812d28bebcf39854be17cfbd4c45cae55a686443
Noman Ali <132977453+space551@users.noreply.github.com>
2024-12-26 16:53:25 +0500

    User Profile

--- lib/widgets/numbers_widget.dart
+++ lib/widgets/numbers_widget.dart
@@ -12,6 +12,7 @@ class NumbersWidget extends StatelessWidget {
       buildButton(context, '50', 'Followers'),
     ],
   );
+
   Widget buildDivider() => Container(
     height: 24,
     child: VerticalDivider(),
