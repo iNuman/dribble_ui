0dae299baf45ecf75c06bd69ca9abc3833114a50
Noman Ali <132977453+space551@users.noreply.github.com>
2024-12-26 16:43:53 +0500

    User Profile

---
+++ lib/widgets/app_bar_widget.dart
@@ -0,0 +1,33 @@
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
+import 'package:dribble_ui/data/mock_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../themes/themes.dart';
+AppBar buildAppBar(BuildContext context) {
+  const user = MockData.userData;
+  final isDarkMode = user.isDarkMode;
+  final icon = isDarkMode ? CupertinoIcons.sun_max : CupertinoIcons.moon_stars;
+
+  return AppBar(
+    // leading: BackButton(),
+    backgroundColor: Colors.transparent,
+    elevation: 0,
+    actions: [
+      ThemeSwitcher(
+        builder: (context) => IconButton(
+          icon: Icon(icon),
+          onPressed: () {
+            final theme = isDarkMode ? MyThemes.lightTheme : MyThemes.darkTheme;
+
+            final switcher = ThemeSwitcher.of(context);
+            switcher.changeTheme(theme: theme);
+
+            final newUser = user.copy(isDarkMode: !isDarkMode);
+            // UserPreferences.setUser(newUser);
+          },
+        ),
+      ),
+    ],
+  );
+}
