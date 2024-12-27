41c58ea0204ab156210ae51dd0e50ef5f2b56f5b
Noman Ali <132977453+space551@users.noreply.github.com>
2024-12-27 11:19:55 +0500

    Theme switch setup

--- lib/data/mock_data.dart
+++ lib/data/mock_data.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import '../models/user.dart';
 
 class MockData {
-  static const userData = User(
+  static User userData = const User(
     imagePath:
     'https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80',
     name: 'Noman Ali',
@@ -12,5 +12,12 @@ class MockData {
     'Skilled Android and Application Developer with years of experience designing and building user-friendly, high-performance mobile apps tailored to meet individual client needs and deliver seamless digital solutions.',    isDarkMode: false,
   );
 
+  static void setUser(User newUser) {
+    userData = newUser;
+  }
+
+  // static void toggleDarkMode() {
+  //   userData = userData.copyWith(isDarkMode: !userData.isDarkMode);
+  // }
 }
 
