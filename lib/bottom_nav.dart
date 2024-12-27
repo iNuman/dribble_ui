415ef08ee8076ffa3f52da4b0d815e7876f6b7d2
Noman Ali <132977453+space551@users.noreply.github.com>
2024-12-27 12:57:36 +0500

    Custom bottom nav

--- lib/bottom_nav.dart
+++ lib/bottom_nav.dart
@@ -34,21 +34,113 @@ class _BottomNavAppState extends State<BottomNavApp> {
       child: ThemeSwitchingArea(
         child: Builder(
           builder: (context) => Scaffold(
-            backgroundColor: Colors.blue.shade800,
+            backgroundColor: Colors.white,
             body: Navigator(
               initialRoute: "Home",
               key: _navigatorKey,
               onGenerateRoute: _generateRoute,
             ),
-            bottomNavigationBar: BottomNavigationBar(
-              items: const [
-                BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-                BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Smart Home'),
-                BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-              ],
-              currentIndex: _currentTabIndex,
-              onTap: _onTap,
-            ),
+            bottomNavigationBar: Container(
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade800,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                  IconButton(
+                    enableFeedback: true,
+                    onPressed: () {
+                      setState(() {
+                        _currentTabIndex = 0;
+                        _onTap(_currentTabIndex);
+                      });
+                    },
+                    icon: _currentTabIndex == 0
+                        ? const Icon(
+                            Icons.home_filled,
+                            color: Colors.white,
+                            size: 35,
+                          )
+                        : const Icon(
+                            Icons.home_outlined,
+                            color: Colors.white,
+                            size: 35,
+                          ),
+                  ),
+                  IconButton(
+                    enableFeedback: false,
+                    onPressed: () {
+                      setState(() {
+                        _currentTabIndex = 1;
+                        _onTap(_currentTabIndex);
+                      });
+                    },
+                    icon: _currentTabIndex == 1
+                        ? const Icon(
+                            Icons.work_rounded,
+                            color: Colors.white,
+                            size: 35,
+                          )
+                        : const Icon(
+                            Icons.work_outline_outlined,
+                            color: Colors.white,
+                            size: 35,
+                          ),
+                  ),
+                  IconButton(
+                    enableFeedback: false,
+                    onPressed: () {
+                      setState(() {
+                        _currentTabIndex = 2;
+                        _onTap(_currentTabIndex);
+                      });
+                    },
+                    icon: _currentTabIndex == 2
+                        ? const Icon(
+                      Icons.person,
+                      color: Colors.white,
+                      size: 35,
+                    )
+                        : const Icon(
+                      Icons.person_outline,
+                      color: Colors.white,
+                      size: 35,
+                    ),
+                  ),
+                  // IconButton(
+                  //   enableFeedback: false,
+                  //   onPressed: () {
+                  //     setState(() {
+                  //       _currentTabIndex = 3;
+                  //       _onTap(_currentTabIndex);
+                  //     });
+                  //   },
+                  //   icon: _currentTabIndex == 3
+                  //       ? const Icon(
+                  //           Icons.widgets_rounded,
+                  //           color: Colors.white,
+                  //           size: 35,
+                  //         )
+                  //       : const Icon(
+                  //           Icons.widgets_outlined,
+                  //           color: Colors.white,
+                  //           size: 35,
+                  //         ),
+                  // ),
+
+                ])),
+            // bottomNavigationBar: BottomNavigationBar(
+            //   items: const [
+            //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            //     BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Smart Home'),
+            //     BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+            //   ],
+            //   currentIndex: _currentTabIndex,
+            //   onTap: _onTap,
+            // ),
           ),
         ),
       ),
